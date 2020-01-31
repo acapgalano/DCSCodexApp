33a22bc3c51f36830d58edb3410790835ce1df31
theswinghobo <acapgalano@gmail.com>
2020-01-31 18:30:49 +0800

    Add DCS Codex mobile side, add Sprint 1 documents

---
+++ DCS Codex App Mobile/DCSCodex/app/src/main/java/com/example/dcscodex/Main3Activity.java
@@ -0,0 +1,17 @@
+package com.example.dcscodex;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ DCS Codex App Mobile/DCSCodex/app/src/main/java/com/example/dcscodex/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.dcscodex.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
